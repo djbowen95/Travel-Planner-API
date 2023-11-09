4b453b0f637ded9f3908e6b01b07c588e13fed49
Daniel Bowen <djbowen95@gmail.com>
2023-11-09 18:29:10 +0000

    Error handling for Express connection

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const PORT = 3000; // Will change.
 
-console.log("Server file")
-
-app.listen(3000); // Need to change this. 
+app.listen(PORT, (error) => {
+    if(!error) {
+        console.log("Successfully listening on PORT" + PORT);
+    } else {
+        console.log("Error occured, server cannot start.");
+        console.log(error);
+    }
+}); // Need to change this. 
 
 // Tell it where to listen
 // Start express
