24ccb9b6aeb5b701c70464afa00e3a581bb249d6
Daniel Bowen <djbowen95@gmail.com>
2023-11-09 19:15:45 +0000

    Modularize connection

--- server.js
+++ server.js
@@ -1,41 +1,9 @@
 const express = require("express");
-const { Sequelize } = require("sequelize");
-const mysql = require("mysql2/promise");
-
-require("dotenv").config();
+const { connection } = require("./config/connection");
 
 const app = express();
 const PORT = 3000; // Will change.
-
-mysql
-  .createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  })
-  .then((connection) => {
-    connection
-      .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`)
-      .then(() => {
-        const sequelize = new Sequelize(
-          process.env.DB_NAME,
-          process.env.DB_USER,
-          process.env.DB_PASSWORD,
-          {
-            host: "localhost",
-            dialect: "mysql",
-          }
-        );
-
-        try {
-          sequelize.authenticate();
-          console.log(
-            "Connection has been established successfully with the database."
-          );
-        } catch (error) {
-          console.error("Unable to connect to the database:", error);
-        }
-      });
-  });
+connection()
 
 app.listen(PORT, (error) => {
   if (!error) {
