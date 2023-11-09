5f8ddb94a8abd71eec37085fd06deff46ce3a583
Daniel Bowen <djbowen95@gmail.com>
2023-11-09 19:07:54 +0000

    Create the database if it does not exist, using mysql package

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const { Sequelize } = require("sequelize");
+const mysql = require('mysql2/promise');
 
 require("dotenv").config();
 
 const app = express();
 const PORT = 3000; // Will change.
 
-const sequelize = new Sequelize(
-  'library_db',
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
+mysql.createConnection({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+}).then((connection) => {
+    connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`).then(()=>{
+        const sequelize = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASSWORD,
+            {
+              host: "localhost",
+              dialect: "mysql",
+            }
+          );
+
+          try {
+            sequelize.authenticate();
+            console.log(
+              "Connection has been established successfully with the database."
+            );
+          } catch (error) {
+            console.error("Unable to connect to the database:", error);
+          }
+          
+    })
+})
+
+
+
 
 app.listen(PORT, (error) => {
   if (!error) {
@@ -25,14 +46,6 @@ app.listen(PORT, (error) => {
   }
 }); // Need to change this.
 
-try {
-  sequelize.authenticate();
-  console.log(
-    "Connection has been established successfully with the database."
-  );
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
 
 // Tell it where to listen
 // Start express
