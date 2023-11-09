ef7e3f7000053f2250c7754c60b0f243ce4e45fc
Daniel Bowen <djbowen95@gmail.com>
2023-11-09 18:25:00 +0000

    Initialize basic Express app

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+console.log("Server file")
+
+app.listen(3000); // Need to change this. 
+
+// Tell it where to listen
+// Start express
