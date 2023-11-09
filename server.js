4b02a8433e971d194b62ad48e67a0dec5fa22b59
Daniel Bowen <djbowen95@gmail.com>
2023-11-09 19:10:31 +0000

    Tidy server.js

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
 const express = require("express");
 const { Sequelize } = require("sequelize");
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 require("dotenv").config();
 
 const app = express();
 const PORT = 3000; // Will change.
 
-mysql.createConnection({
+mysql
+  .createConnection({
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-}).then((connection) => {
-    connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`).then(()=>{
+    password: process.env.DB_PASSWORD,
+  })
+  .then((connection) => {
+    connection
+      .query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`)
+      .then(() => {
         const sequelize = new Sequelize(
-            process.env.DB_NAME,
-            process.env.DB_USER,
-            process.env.DB_PASSWORD,
-            {
-              host: "localhost",
-              dialect: "mysql",
-            }
-          );
-
-          try {
-            sequelize.authenticate();
-            console.log(
-              "Connection has been established successfully with the database."
-            );
-          } catch (error) {
-            console.error("Unable to connect to the database:", error);
+          process.env.DB_NAME,
+          process.env.DB_USER,
+          process.env.DB_PASSWORD,
+          {
+            host: "localhost",
+            dialect: "mysql",
           }
-          
-    })
-})
+        );
 
-
-
+        try {
+          sequelize.authenticate();
+          console.log(
+            "Connection has been established successfully with the database."
+          );
+        } catch (error) {
+          console.error("Unable to connect to the database:", error);
+        }
+      });
+  });
 
 app.listen(PORT, (error) => {
   if (!error) {
@@ -44,8 +44,4 @@ app.listen(PORT, (error) => {
     console.log("Error occured, server cannot start.");
     console.log(error);
   }
-}); // Need to change this.
-
-
-// Tell it where to listen
-// Start express
+});
