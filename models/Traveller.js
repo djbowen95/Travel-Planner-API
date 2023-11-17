1575a542fec8aad453c9e47ab88f9d9e6243c084
Daniel Bowen <djbowen95@gmail.com>
2023-11-17 23:18:07 +0000

    Create a Traveller model with all three properties defined

--- models/Traveller.js
+++ models/Traveller.js
@@ -1,1 +1,42 @@
 // Traveller model here
+const { Sequelize, DataTypes, Model } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
+
+class Traveller extends Model {}
+
+Traveller.init(
+  {
+    traveller_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Traveller",
+  }
+);
+
+module.exports = Traveller;
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const { connection } = require("./config/connection");
+const { Sequelize } = require("sequelize");
 
 const app = express();
 const PORT = 3000; // Will change.
 connection();
 
-app.listen(PORT, (error) => {
-  if (!error) {
-    console.log("Successfully listening on PORT" + PORT);
-  } else {
-    console.log("Error occured, server cannot start.");
-    console.log(error);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
   }
+);
+
+const Traveller = require("./models/Traveller");
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, (error) => {
+    if (!error) {
+      console.log("Successfully listening on PORT" + PORT);
+    } else {
+      console.log("Error occured, server cannot start.");
+      console.log(error);
+    }
+  });
 });
