0d42b7094405e6b328ea8bcc92e07ad4a4ec7898
Daniel Bowen <djbowen95@gmail.com>
2023-11-17 21:16:24 +0000

    Add extra whitespace to break/test linting rule

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { connection } = require("./config/connection");
 
-const app = express();
+const app =  express();
 const PORT = 3000; // Will change.
 connection();
 
